53bcb8b6b5f7eb3d37779a6a677335111affb047
Mykhaylo Glushakov <mykhayloglushakov@iPod-touch.localdomain>
2020-04-08 21:30:59 -0400

    some wertc stuff

---
+++ public/js/index.js
@@ -0,0 +1,156 @@
+console.log("hello there");
+const socket=io();
+var myId;
+let buddy={};
+let isConnected=false;
+let button;
+let audio;
+let video;
+let pc;
+let localStream;
+
+const constraints = window.constraints = {
+  audio: true,
+  video: true
+};
+const name = "Player-"+Math.floor(Math.random() * Math.floor(1000))+"-"+Math.floor(Math.random() * Math.floor(1000))+"-"+Math.floor(Math.random() * Math.floor(1000));
+
+socket.on('connect',function(){
+  console.log('SOCKETIO connected');
+  button= document.getElementById("call-button");
+  audio= document.getElementById('video1');
+  button.onclick=callNeighbor;
+  socket.emit('player connect',{name,translation:[0,0,0],rotation:[0,0,0]});
+});
+
+socket.on('NewPlayer',function(data){
+  console.log('SOCKETIO NewPlayer');
+  console.log(JSON.stringify(data));
+  buddy=data;
+  button.disabled=false;
+
+})
+
+socket.on('PlayerDisconnect',function(data){
+  console.log(`SOCKETIO ${data.name} disconnected`);
+  buddy={};
+  button.disabled=true;
+})
+
+socket.on('rtcRequest',function(data){
+  console.log(`RTC_REQUEST from ${data.from} to ${data.to}`);
+  console.log(`RTC_REQUEST body: ${data.body}`);
+
+  const offer=data.body;
+  const servers = null;
+  pc=new RTCPeerConnection(servers);
+  pc.onicecandidate = e => onIceCandidate(pc, e);
+  pc.ontrack=gotRemoteStream;
+  pc.setRemoteDescription(data.body).then(success=>{
+    console.log('SET DESCRIPTION');
+    console.log(pc.remoteDescription);
+    pc.createAnswer().then(answer=>{
+      pc.setLocalDescription(answer).then(success=>{
+          socket.emit('rtcResponse',{from:name,to:buddy.name,body:pc.localDescription});
+      },error=>{console.log(error)})
+
+    },error=>{console.log(error)});},
+    error=>{console.log(error)})
+
+})
+
+socket.on('rtcResponse',function(data){
+  console.log(`RTC_RESPONSE from ${data.from} to ${data.to}`);
+  console.log(`RTC_RESPONSE body: ${data.body}`);
+  pc.setRemoteDescription(data.body).then(success=>{
+    console.log('success')
+  },error=>console.log(error))
+
+
+})
+
+function callNeighbor(){
+  if(buddy=={}){console.log('CALL_NEIGHBOR Neighbor NULL')}
+  else{
+
+    const servers = null;
+    pc=new RTCPeerConnection(servers);
+    pc.onicecandidate = e => onIceCandidate(pc, e);
+    pc.ontrack=gotRemoteStream;
+
+    console.log('Created local peer connection object pc1');
+    navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
+    // socket.emit('rtcRequest',{from:name,to:buddy.name,body:"So Call me maybe"});
+  }
+}
+
+
+function handleSuccess(stream) {
+  console.log(stream);
+  audio.srcObject = stream;
+
+  localStream = stream;
+  const audioTracks = localStream.getAudioTracks();
+  if (audioTracks.length > 0) {
+    console.log(`Using Audio device: ${audioTracks[0].label}`);
+  }
+  localStream.getTracks().forEach(track => pc.addTrack(track, localStream));
+  console.log('Adding Local Stream to peer connection');
+  pc.createOffer().then(success=>{
+    console.log('RTC SUCCESS')
+    console.log(success)
+    pc.setLocalDescription(success).then(success=>{
+      socket.emit('rtcRequest',{from:name,to:buddy.name,body:pc.localDescription});
+
+    },error=>{
+      console.log('RTC FAILED to set description');
+      console.log(failure);
+    });
+
+  },
+    failure=>{
+      console.log('RTC FAILED to create offer');
+      console.log(failure);
+    })
+  // window.stream = stream; // make variable available to browser console
+  // audio.srcObject = stream;
+}
+
+function handleError(error) {
+  const errorMessage = 'navigator.MediaDevices.getUserMedia error: ' + error.message + ' ' + error.name;
+  // errorMsgElement.innerHTML = errorMessage;
+  console.log(errorMessage);
+}
+
+
+function onIceCandidate(pc, event) {
+  console.log('**ICE** '+event.candidate)
+  pc.addIceCandidate(event.candidate)
+      .then(
+          () => onAddIceCandidateSuccess(pc),
+          err => onAddIceCandidateError(pc, err)
+      );
+  console.log(`PC ICE candidate:\n${event.candidate ? event.candidate.candidate : '(null)'}`);
+}
+
+function onAddIceCandidateSuccess() {
+  console.log('AddIceCandidate success.');
+}
+
+function onAddIceCandidateError(error) {
+  console.log(`Failed to add ICE Candidate: ${JSON.stringify(error)}`);
+}
+
+function onSetSessionDescriptionError(error) {
+  console.log(`Failed to set session description: ${(error).toString()}`);
+}
+
+function gotRemoteStream(e) {
+  // console.log(e.streams[0]);
+  // if (audio.srcObject !== e.streams[0]) {
+  //   audio.srcObject = e.streams[0];
+  //   console.log('Received remote stream');
+  // }
+  console.log(e.streams);
+  audio.srcObject=e.streams[0];
+}
---
+++ server.js
@@ -0,0 +1,42 @@
+const express=require('express');
+const path =require('path');
+const app=express();
+const server=require('http').Server(app);
+const io=require('socket.io')(server);
+const publicDirectoryPath = path.join(__dirname, '/public');
+
+
+app.get("/ping",(req,res)=>{res.send("PING");})
+app.use(express.static(publicDirectoryPath));
+server.listen(process.env.PORT||3000);
+
+let clients=[];
+io.on('connection',function(socket){
+  const currentPlayer={};
+  currentPlayer.name='unknown';
+
+  socket.on('player connect',function(player){
+  for(let i=0;i<clients.length;i++){
+        let client = JSON.parse(JSON.stringify(clients[i]));
+        socket.emit("NewPlayer",(client));
+  }
+  socket.broadcast.emit("NewPlayer",player)
+  clients.push(JSON.parse(JSON.stringify(player)));
+  currentPlayer.name=JSON.parse(JSON.stringify((player))).name;
+
+})
+socket.on('disconnect',function(){
+  for(let i=0;i<clients.length;i++){
+    if(clients[i].name==currentPlayer.name){clients.splice(i,1);}
+  }
+  socket.broadcast.emit("PlayerDisconnect",{name:currentPlayer.name});
+})
+
+socket.on('rtcRequest',function(data){
+  socket.broadcast.emit('rtcRequest',data);
+})
+socket.on('rtcResponse',function(data){
+  socket.broadcast.emit('rtcResponse',data);
+})
+
+})
